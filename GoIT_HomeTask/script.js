9f6d58a8710d1f72cb02ea7b5c8b1e9fffdcddec
Kostia Shmyhelskyi <K.Shmyhelskyi@gmail.com>
2017-01-13 13:48:36 +0200

    second version

--- GoIT_HomeTask/script.js
+++ GoIT_HomeTask/script.js
@@ -2,9 +2,12 @@
 var a = prompt ('Укажите число');
 var b = prompt ('Укажите степень');
 
-function pow () {
- 	var result = Math.pow (a , b);
-	alert (result);
+function pow(x, n) {
+  if (n != 1) { 
+    return x * pow(x, n - 1);
+  } else {
+    return x;
+  }
 }
 pow ();
 
